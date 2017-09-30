2d0a9f99873b4fce85a9a3094494656065237eef
Sara Hopper <sara.hopper@gmail.com>
2017-09-30 17:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,16 +31,16 @@ class Passenger {
     store.passengers.push(this)
   }
   trips(){
-      return store.trips.filter((trip)=> {
-        return trip.passengerId == this.id
-      })
-    }
-    drivers(){
-      return this.trips().map((trip)=> {
-        return trip.driver()
-      })
-    }
+    return store.trips.filter((trip)=> {
+      return trip.passengerId == this.id
+    })
   }
+  drivers(){
+    return this.trips().map((trip)=> {
+      return trip.driver()
+    })
+  }
+}
 
 
 class Trip {
