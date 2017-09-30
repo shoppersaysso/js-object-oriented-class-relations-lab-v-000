99354c1baf1776bb53c777fd1d419b866adab970
Sara Hopper <sara.hopper@gmail.com>
2017-09-30 17:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class Passenger {
   }
   trips(){
     return store.trips.filter(function(trip) {
-      return trip.passengerId === this.id
+      return trip.passengerId == this.id
     })
   }
 }
