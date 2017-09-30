308a88fff59f2709b1735879d6610b25f0b61fd5
Sara Hopper <sara.hopper@gmail.com>
2017-09-30 17:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class Trip {
   }
   driver(){
     return store.drivers.find((driver) => { return driver.id === this.driverId })
-    }
+  }
   passenger(){
     return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
   }
