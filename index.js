61f4a04b5296b0d06d7a0451c44f827b38b3b335
Sara Hopper <sara.hopper@gmail.com>
2017-09-30 17:22:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 let store = {drivers: [], passengers: [], trips: []}
 
 let driverId = 0
+let passengerId = 0
+let tripId = 0
+
 class Driver {
-  constructor(name, zipcode){
-    this.name = name
+  constructor(name){
     this.id = ++driverId
+    this.name = name
+
     store.drivers.push(this)
   }
   trips(){
-    return store.trips.filter((trip)=> {
+    return store.trips.filter(function(trip) {
       return trip.driverId == this.id
     })
   }
   passengers(){
-    return this.trips().map((trip)=> {
+    return this.trips().map(function(trip) {
       return trip.passenger()
     })
   }
 }
 
-let passengerId = 0
-
 class Passenger {
   constructor(name){
-    this.name = name
     this.id = ++passengerId
+    this.name = name
+
     store.passengers.push(this)
   }
-
+  setPassenger(passenger){
+    this.passengerId = passenger.id
+  }
   trips(){
-    return store.trips.filter((trip)=> {
+    return store.trips.filter(function(trip) {
       return trip.passengerId == this.id
     })
   }
   drivers(){
-    return this.trips().map((trip)=> {
+    return this.trips().map(function(trip) {
       return trip.driver()
     })
   }
 }
 
-let tripId = 0
 class Trip {
-  constructor(driver, passenger, startingZip, endingZip){
-    this.driverId = driverId;
-    this.passengerId = passengerId;
+  constructor(driverId, passengerId){
     this.id = ++tripId
+    this.driverId = driverId
+    this.passengerId = passengerId
+
     store.trips.push(this)
   }
   driver(){
